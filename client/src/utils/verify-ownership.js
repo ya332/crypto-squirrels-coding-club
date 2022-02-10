a8019496352e1c64dd9111b6be3097d2571a69b6
ya332 <26026196+ya332@users.noreply.github.com>
2022-02-10 13:22:41 -0800

    Fix typos

--- client/src/utils/verify-ownership.js
+++ client/src/utils/verify-ownership.js
@@ -12,7 +12,7 @@ export async function verifyOwnership(userOwnedTokens){
     const data = await response.json();
     let storeOwnedTokenIds = [];
     for (let idx in data.store[0].things){
-        storeOwnedTokenIds.push(data.store[0].things[idx].id) // MTL4o-0kxiMleORXJaHexCZHkN63PVH7L4ZMeVto-oQ:cryptosquirrelsclub.mintspace2.testnet
+        storeOwnedTokenIds.push(data.store[0].things[idx].id) // i.e MTL4o-0kxiMleORXJaHexCZHkN63PVH7L4ZMeVto-oQ:cryptosquirrelsclub.mintspace2.testnet
     }
     for (let idx in userOwnedTokens){
         if (storeOwnedTokenIds.includes(userOwnedTokens[idx].thing['id'])){
